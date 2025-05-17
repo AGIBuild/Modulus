39b03402536a97db95f9f7f3a6913a31eb628225
Hongwei Xi <Hongwei.Xi@myob.com>
2025-05-17 14:46:14 +0800

    change code structure

---
+++ src/Modulus.App/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Modulus.App.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ src/Modulus.App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Modulus.App.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
