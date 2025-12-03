2c3c4b4bad4afa9620978d19e4014bf2afdea011
Hongwei.Xi <hongwei.xi@myob.com>
2025-12-03 20:12:25 +0800

    refactor: use NavigationView component from Modulus.UI.Avalonia
    - Replace custom navigation implementation with NavigationView component
    - Simplify ComponentsMainView to use nav:NavigationView
    - Handle ItemSelected event for navigation

--- src/Modules/ComponentsDemo/ComponentsDemo.UI.Avalonia/ComponentsMainView.axaml.cs
+++ src/Modules/ComponentsDemo/ComponentsDemo.UI.Avalonia/ComponentsMainView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Modulus.Modules.ComponentsDemo.ViewModels;
+using UiMenuItem = Modulus.UI.Abstractions.MenuItem;
 
 namespace Modulus.Modules.ComponentsDemo.UI.Avalonia;
 
@@ -7,6 +9,14 @@ public partial class ComponentsMainView : UserControl
     public ComponentsMainView()
     {
         InitializeComponent();
+    }
+
+    private void OnNavigationItemSelected(object? sender, UiMenuItem item)
+    {
+        if (DataContext is ComponentsMainViewModel vm && !string.IsNullOrEmpty(item.NavigationKey))
+        {
+            vm.NavigateTo(item.NavigationKey);
+        }
     }
 }
 
